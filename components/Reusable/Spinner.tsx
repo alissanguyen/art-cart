31eacc7d821ed8ca89999f1456d0cd55db5774c2
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2020-12-19 08:19:10 +0100

    Add loading spinner.

---
+++ components/Reusable/Spinner.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const Spinner: React.FC = ({ }) => {
+ return (
+    <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+ )
+}
+
+export default Spinner
---
+++ components/loadingStates/UserLoadingState.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface Props {
+
+}
+
+const UserLoadingState: React.FC<Props> = ({ }) => {
+    
+ return (
+ <div>Hi</div>
+ )
+}
+
+export default UserLoadingState
